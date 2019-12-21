dfb5342c0fd6024a382ad1b66244e30e289f83ab
Dmitry Egorov <degorov@greenminds.ru>
2019-12-21 14:06:50 +0300

    `utils\detect` fully covered

--- spec/detect/index/spec-01.js
+++ spec/detect/index/spec-01.js
@@ -16,6 +16,13 @@ module.exports = (dd) => {
         e: {foo: 42, bar: 'two'},
       }]);
     });
+    dd.context('when detector is not found', () => {
+      dd.drive([{
+        it: 'returns default output',
+        i: [input, {}],
+        e: [{}, false],
+      }]);
+    });
   });
   dd.context('With invalid arguments', () => {
     dd.context('with first argument which could not be properly hadled by detectors', () => {

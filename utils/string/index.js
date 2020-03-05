ad629bc3f119b83947f6f8db72cf6c32a86a5dda
closure-reference <appendix.y.z@gmail.com>
2020-03-05 16:45:16 +0300

    utils/string WIP

--- utils/string/index.js
+++ utils/string/index.js
@@ -2,12 +2,12 @@ const repeat = (count, pattern) => Array(count).fill(pattern).join('');
 
 module.exports = {
   capitalize: (string) => string.replace(
-    /\w\S*/g,
-    (txt) => `${txt.charAt(0).toUpperCase()}${txt.substr(1)}`
+    /\b\w/g,
+    (letter) => letter.toUpperCase()
   ),
   indent: (count, pattern, subject) => `${repeat(count, pattern)}${subject}`.slice(-count),
   repeat,
   stripSpaces: (string) => string
-    .replace(/^\s+|\s+$/, '')
-    .replace(/\s{1}\s+/g, ' '),
+  .replace(/\s+/g, ' ')
+  .replace(/^ | $/g, ''),
 };
